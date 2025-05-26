d22517adfad945199743c042518e93ccd8748792
abrilzacarias <abrilzacarias6@gmail.com>
2025-05-26 16:22:04 -0300

    refactor

--- backend/app/models/tiporepuesto.py
+++ backend/app/models/tiporepuesto.py
@@ -1,14 +1,11 @@
-from typing import List, Optional
-from sqlalchemy import DECIMAL, Date, ForeignKeyConstraint, Index, Integer, JSON, String, text
-from sqlalchemy.dialects.mysql import TINYINT
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 class TipoRepuesto(Base):
-    __tablename__ = 'tipoRepuesto'
+    __tablename__ = "tipoRepuesto"
 
-    idTipoRepuesto = mapped_column(Integer, primary_key=True)
-    descripcionTipoRespuesto = mapped_column(String(80))
+    idTipoRepuesto = Column(Integer, primary_key=True, index=True)
+    descripcionTipoRepuesto = Column(String(80), nullable=False)
 
-    repuesto = relationship('Repuesto', back_populates='tipoRepuesto_')
-
+    repuestos = relationship("Repuesto", back_populates="tipo")
