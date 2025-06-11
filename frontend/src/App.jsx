0e84da594072400b9d08fd24b3161ab0396b1cd6
VictoriaVMC <94030658+victoriavmc@users.noreply.github.com>
2025-06-11 00:39:41 -0300

    Empleado Elimina correctamente, con modal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import ReparacionesPage from "./pages/reparaciones/ReparacionesPage"
 import ClientePage from "./pages/cliente/ClientePage"
 import PerfilesPage from "./pages/perfiles/PerfilesPage"
 import AddPerfilPage from "./pages/perfiles/AddPerfilPage"
+// import MarcasPage from "./pages/marcas/MarcasPage"
 
 function App() {
   return (
@@ -26,15 +27,15 @@ function App() {
         >
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/diagnosticos" element={<DiagnosticoPage />} />
-          {/* 
-          <Route path="/reparaciones" element={<ReparacionesPage />} /> */}
+          <Route path="/empleados" element={<EmpleadoPage />} /> 
           <Route path="/perfiles" element={<PerfilesPage />} />
           <Route path="/perfiles/nuevo" element={<AddPerfilPage />} />
-          <Route path="/empleados" element={<EmpleadoPage />} />
+          <Route path="/reparaciones" element={<ReparacionesPage />} />
           <Route path="/clientes" element={<ClientePage />} />
           <Route path="/repuestos" element={<RepuestosPage />} />
           <Route path="/configuracion" element={<div>Página de Configuración</div>} />
           <Route path="/reparaciones" element={<ReparacionesPage />} />
+          <Route path="/marcas" element={<MarcasPage/>} />
         </Route>
 
         <Route path="*" element={<Navigate to="/dashboard" />} />
